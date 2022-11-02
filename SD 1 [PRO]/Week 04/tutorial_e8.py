bb7dccc499977c9e6b5aadb579717494aa64e51c
hashankur <aeonian.adroit24@gmail.com>
2022-11-02 08:16:48 +0530

    Improved tutorial_e8.py | Week 06 lecture

--- SD 1 [PRO]/Week 04/tutorial_e8.py
+++ SD 1 [PRO]/Week 04/tutorial_e8.py
@@ -1,7 +1,4 @@
-i = 1
-
-for x in range(1, 4):  # print 3 rows
-    i += 1
-    for y in range(1, i):  # (i + 1) asterisks a row
+for x in range(1, 4):  # print 3 
+    for y in range(x):  # (x) asterisks a row
         print("*", end=" ")
     print()
