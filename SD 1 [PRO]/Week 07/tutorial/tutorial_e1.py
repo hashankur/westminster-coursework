b8fce3a053c17e910fb3e16b774915557cb6bc7f
hashankur <aeonian.adroit24@gmail.com>
2022-11-16 20:14:50 +0530

    weeks 07, 08

---
+++ SD 1 [PRO]/Week 07/tutorial/tutorial_e1.py
@@ -0,0 +1,12 @@
+list_a = ['abc', 100, 3.14, 'def']
+
+print(list_a)
+
+print(f"A. {list_a[0]}")
+print(f"B. {list_a[2:4]}")
+
+list_a.insert(2, 35)
+print(f"C. {list_a}")
+
+list_a.append(90)
+print(f"D. {list_a}")
