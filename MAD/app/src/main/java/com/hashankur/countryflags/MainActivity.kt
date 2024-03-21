33af0922a533c415e499022a0d52eca50e77d930
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-03-21 15:21:04 +0530

    feat(MAD): improve UX in MenuScreen

--- MAD/app/src/main/java/com/hashankur/countryflags/MainActivity.kt
+++ MAD/app/src/main/java/com/hashankur/countryflags/MainActivity.kt
@@ -13,10 +13,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -114,7 +114,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun TopBarBuilder(title: String, navController: NavHostController) {
-        MediumTopAppBar(
+        TopAppBar(
             colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primary,
                 titleContentColor = MaterialTheme.colorScheme.onPrimary
--- MAD/app/src/main/java/com/hashankur/countryflags/ui/MenuScreen.kt
+++ MAD/app/src/main/java/com/hashankur/countryflags/ui/MenuScreen.kt
@@ -1,23 +1,38 @@
 package com.hashankur.countryflags.ui
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MenuButton(name: String, onClick: () -> Unit) {
     Button(
         onClick,
-        Modifier.width(250.dp)
+        Modifier
+            .width(400.dp)
+            .height(100.dp)
+            .padding(20.dp)
     )
     {
-        Text(name)
+        Text(name, fontSize = 20.sp)
     }
 }
 
@@ -30,8 +45,26 @@ fun MenuScreen(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
+        val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+        val animatedColor by infiniteTransition.animateColor(
+            initialValue = MaterialTheme.colorScheme.primary,
+            targetValue = MaterialTheme.colorScheme.secondary,
+            animationSpec = infiniteRepeatable(tween(2000), RepeatMode.Reverse),
+            label = "color"
+        )
+        Text(
+            "Choose\nYour\nMode.",
+            modifier = Modifier
+                .padding(horizontal = 50.dp)
+                .padding(bottom = 50.dp)
+                .fillMaxWidth(),
+            fontSize = 64.sp,
+            lineHeight = 80.sp,
+            fontWeight = FontWeight.Bold,
+            color = animatedColor // https://developer.android.com/jetpack/compose/animation/quick-guide#animate-text-color
+        )
         MenuButton("Guess the Country", onClick = onNavigateToGuessCountry)
         MenuButton("Guess Hints", onClick = onNavigateToGuessHints)
         MenuButton("Guess the Flag", onClick = onNavigateToGuessFlag)
